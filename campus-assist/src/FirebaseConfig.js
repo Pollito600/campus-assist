3e8c3b20a123b003655d6ce1c1b63f88d55b3135
rxs8957 <rxs8957@mavs.uta.edu>
2024-03-12 11:47:31 -0500

    new firebase config base on new general google acct

--- campus-assist/src/FirebaseConfig.js
+++ campus-assist/src/FirebaseConfig.js
@@ -1,20 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDrfC5tyeYzkBWNbroj9LifeP7ob8dd9-s",
-  authDomain: "campus-assist-b4220.firebaseapp.com",
-  projectId: "campus-assist-b4220",
-  storageBucket: "campus-assist-b4220.appspot.com",
-  messagingSenderId: "811129780495",
-  appId: "1:811129780495:web:ea1f0dfc2a642bc904267a"
+  apiKey: "AIzaSyDPPjsbvbwjYfqteuAouGpcAxRcWwpFyvM",
+  authDomain: "campus-assist-49dde-6e38e.firebaseapp.com",
+  projectId: "campus-assist-49dde",
+  storageBucket: "campus-assist-49dde.appspot.com",
+  messagingSenderId: "157729141933",
+  appId: "1:157729141933:web:11af78fcacd6a0ab304224",
+  measurementId: "G-J4HVPE4CNH"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const auth = getAuth(app)
 export {auth};
